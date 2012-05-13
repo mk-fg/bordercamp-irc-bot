88c0b0b883f9685ced2896bc7604331a992d3db4
Mike Kazantsev <mk.fraggod@gmail.com>
2012-05-13 19:46:14 +0600

    relays.base: added dummy dispatch method

--- bordercamp/relays/__init__.py
+++ bordercamp/relays/__init__.py
@@ -5,3 +5,6 @@ class BCRelay(object):
 
 	def __init__(self, conf, interface):
 		self.conf, self.interface = conf, interface
+
+	def dispatch(self, msg):
+		raise ValueError('This relay is not designed to process incoming messages')
