66301969b702623dee72bf10c74dbb5c14ffcb94
Mike Kazantsev <mk.fraggod@gmail.com>
2013-08-25 19:03:03 +0600

    relays: +pipe_audit_format

---
+++ bordercamp/relays/pipe_audit_format.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+from twisted.python import log
+
+from bordercamp import force_unicode
+from . import BCRelay
+
+import itertools as it, operator as op, functools as ft
+from collections import defaultdict
+import os, re, time
+
+
+class AuditLog(BCRelay):
+
+	_re_base = re.compile(
+		ur'\bnode=(?P<node>\S+)\s+type=(?P<type>[A-Z]+)'
+		ur'\s+msg=audit\((?P<ev_id>[\d.:]+)\):\s*(?P<msg>.*)$' )
+
+	_lookup_error = KeyError, IndexError, AttributeError
+
+	_ev_cache = None
+	_ev_cache_timeout = 3600
+	def _ev_cache_gc(self):
+		if ord(os.urandom(1)) < 10: # 4% chance
+			ts_min = time.time() - self._ev_cache_timeout
+			for k, ev in self._ev_cache.items():
+				ts_ev = ev.get('ts')
+				if ts_ev is not None and ts_ev < ts_min:
+					ev = self._ev_cache.pop(k)
+					log.warn('Unprocessed audit event in cache: {!r}'.format(ev))
+
+	def __init__(self, *argz, **kwz):
+		super(AuditLog, self).__init__(*argz, **kwz)
+		self._ev_cache = dict()
+
+	def get_msg_val(self, msg, k, val=ur'\d+'):
+		match = re.search(ur'\b{}=({})(\s+|$)'.format(re.escape(k), val), msg)
+		if not match: raise KeyError(msg, k)
+		return match.group(1)
+
+	def dispatch(self, msg):
+		if not msg.strip(): return
+
+		match = self._re_base.search(msg)
+		if not match:
+			log.warn('Failed to match audit event spec: {!r}'.format(msg))
+			return
+
+		node, ev_id, ev_type, msg = (match.group(k) for k in ['node', 'ev_id', 'type', 'msg'])
+		ev_key = node, ev_id
+
+		if ev_key not in self._ev_cache:
+			self._ev_cache[ev_key] = defaultdict(list)
+			self._ev_cache[ev_key].update(ts=time.time(), node=node, ev_id=ev_id)
+			self._ev_cache_gc()
+		ev = self._ev_cache[ev_key]
+
+		if ev_type != 'EOE': # cache event data
+			ev[ev_type].append(msg)
+			return
+		del self._ev_cache[ev_key]
+
+		if self.conf.events.watches.enabled:
+			# Extract all necessary attributes
+			ev_vals = dict(node=ev['node'], ev_id=ev['ev_id'])
+			syscall, = ev['SYSCALL']
+			for k in it.imap(''.join, it.product(['', 'e', 's', 'fs'], ['uid', 'gid'])):
+				ev_vals[k] = self.get_msg_val(syscall, k)
+			for k in 'key', 'comm', 'exe':
+				ev_vals[k] = self.get_msg_val(syscall, k, ur'"[^"]+"')
+			ev_vals['tty'] = self.get_msg_val(syscall, 'tty', '\S+')
+			paths = ev_vals['paths'] = list()
+			for msg in ev['PATH']:
+				path = self.get_msg_val(msg, 'name', ur'("[^"]+"|\(null\))')
+				paths.append(dict( path=path,
+					inode=self.get_msg_val(msg, 'inode'),
+					dev=self.get_msg_val(msg, 'dev', '[a-f\d]{2}:[a-f\d]{2}') ))
+
+			# Formatting
+			err, tpl = None, force_unicode(self.conf.events.watches.template_path)
+			ev_vals['paths'] = list()
+			for val in paths:
+				try: ev_vals['paths'].append(tpl.format(**val))
+				except self._lookup_error as err: break
+			if not err:
+				ev_vals['paths'] = ', '.join(ev_vals['paths'])
+				tpl, val = force_unicode(self.conf.events.watches.template), ev_vals
+				try: return tpl.format(**val)
+				except self._lookup_error as err: pass
+			raise ValueError( 'Failed to format template {!r} (data: {}): {}'.format(tpl, val, err))
+
+
+relay = AuditLog
